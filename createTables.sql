2c3f208b8b1fc59161756299e3a438bb17177fed
Dmitry <5418022+dKaryakin@users.noreply.github.com>
2020-06-09 01:13:01 +0600

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -16,3 +16,8 @@ CREATE TABLE pets(
 
 COMMENT ON TABLE usersAndPets.animals IS 'This is a table with animals (dog, cat, hamster, cockroach, etc.)';
 COMMENT ON TABLE usersAndPets.pets IS 'This is a table with users'' animals (we call them pets)';
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE usersandpets.animals TO "user";
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE usersandpets.pets TO "user";
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE usersandpets.users TO "user";
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO www;
